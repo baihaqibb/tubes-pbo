703183be53eeeea0eeab22f8bb86ffe72d723ae5
baihaqibb <138758831+baihaqibb@users.noreply.github.com>
2024-12-08 03:54:56 +0700

    Create Project
    aaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ src/main/java/com/kelompok2/tubespbo/TubespboApplication.java
@@ -0,0 +1,13 @@
+package com.kelompok2.tubespbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubespboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubespboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelompok2/tubespbo/TubespboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelompok2.tubespbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubespboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
