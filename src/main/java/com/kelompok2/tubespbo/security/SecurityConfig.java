ac2c04d620215e44c18087c2ea32b7a74797dd50
baihaqibb <138758831+baihaqibb@users.noreply.github.com>
2024-12-23 19:03:16 +0700

    quickfix again
    i forgor 💀

--- src/main/java/com/kelompok2/tubespbo/security/SecurityConfig.java
+++ src/main/java/com/kelompok2/tubespbo/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/login", "/register")
+                        .requestMatchers("/login", "/register/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
